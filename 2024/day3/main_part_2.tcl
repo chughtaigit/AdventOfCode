a606c03c14379d9161c57a9073fcc03e97e85621
chughtaigit <chughtai@outlook.com>
2025-01-13 12:13:14 -0800

    Day3 Part2 done

--- 2024/day3/main_part_2.tcl
+++ 2024/day3/main_part_2.tcl
@@ -2,8 +2,37 @@ source utility_procs.tcl
 puts "main.tcl"
 set input_file "input.txt"
 #set input_file "example_list.txt"
+#set input_file "demo2.txt"
 set data_dict [parse_file $input_file]
 puts "data_dict: {$data_dict}"
 set safe_cnt [process_data_b $data_dict]
+# Final Resolution
+# ================
+# Real issue was line 100 in utility_procs.tcl where I added a $ in front of the global variable by mistake: 
+# set $IN_DONT_TRACKER $in_dont
+# One this issue got resolved, there was no need to combine all the lines without line breaks!
+
+# Initial Resolution
+# ================
+# ISsue was line breaks! If process individual lines then get the 107991598 answer, but if
+# combine all the lines without line breaks then get the actual answer of 93729253
+# set safe_cnt [process_data_c $data_dict]
+# Right answer: 93729253
+# Previous attempts
 # 107991598
 # Answer too high
+# <0>: 24129420
+# <1>: 25835880
+# <2>: 21798505
+# <3>: 15301835
+# <4>: 16072518
+# <5>: 4853440
+
+# CH
+# <0> Part2 answer: 24129420
+# <1> Part2 answer: 25835880
+# <2> Part2 answer: 21798505
+# <3> Part2 answer: 15301835
+# <4> Part2 answer: 16072518
+# <5> Part2 answer: 4853440
+# puts "Total: [expr 24129420 +25835880 +21798505 +15301835 +16072518 +4853440]"
--- 2024/day3/utility_procs.tcl
+++ 2024/day3/utility_procs.tcl
@@ -74,21 +74,22 @@ proc filter_mul_matches_b { in_matches } {
     set filtered_matches [list]
     set in_dont $IN_DONT_TRACKER
     foreach {whole junk_a junk_b junk_c num_a num_b} $in_matches {
-        #puts "START: whole {$whole} in_dont {$in_dont}"
-        if {$num_a > 999 || $num_b > 999} { 
-            #puts "Filtered: {$whole} >999 $num_a or $num_b" 
-            continue 
-        }
+        puts "START: whole {$whole} in_dont {$in_dont}"
         if {[string match "don't*" $whole]} {
             set in_dont 1
         } elseif {[string match "do()*" $whole]} {
             set in_dont 0
             continue
         }
-        #puts "  END: whole {$whole} in_dont {$in_dont}"
+        puts "  END: whole {$whole} in_dont {$in_dont}"
         if {$in_dont != 0} {
             continue
         }
+        if {$num_a > 999 || $num_b > 999} { 
+            #puts "Filtered: {$whole} >999 $num_a or $num_b" 
+            continue 
+        }
+        puts "  ADDED: whole {$whole} in_dont {$in_dont} num_a {$num_a} num_b {$num_b}"
         lappend filtered_matches $whole
         lappend filtered_matches $junk_a
         lappend filtered_matches $junk_b
@@ -96,7 +97,7 @@ proc filter_mul_matches_b { in_matches } {
         lappend filtered_matches $num_a
         lappend filtered_matches $num_b
     }
-    set $IN_DONT_TRACKER $in_dont
+    set IN_DONT_TRACKER $in_dont
     puts "filtered_matches -> {$filtered_matches}"
     return $filtered_matches
 }
@@ -106,6 +107,7 @@ proc process_mul_matches_b { in_matches } {
     set mul_results 0
     foreach {whole junk_a junk_b junk_c num_a num_b} $in_matches {
         incr mul_results [expr $num_a * $num_b]
+        puts "do it: $num_a * $num_b (answer = $mul_results)"
     }
     return $mul_results
 }
@@ -120,7 +122,31 @@ proc process_data_b { data_dict {use_problem_dampner 0} } {
             puts "  Line $ln_cnt: One or more entries filtered ..."
         }
         set mul_result [process_mul_matches_b $filtered_mul_matches]
-        puts "<$ln_cnt>: $mul_result {$in_str}"
+        #puts "<$ln_cnt>: $mul_result {$in_str}"
+        puts "<$ln_cnt>: $mul_result"
+        incr mul_results $mul_result
+    }
+    puts "mul_results: $mul_results"
+    return $mul_results
+}
+proc process_data_c { data_dict {use_problem_dampner 0} } {
+    global IN_DONT_TRACKER
+    set mul_results 0
+    set IN_DONT_TRACKER 0
+    # Add all lines to one string without line breaks
+    set one_str ""
+    dict for {ln_cnt in_str} $data_dict {
+        append one_str $in_str
+    }
+    dict for {ln_cnt in_str} [list 0 $one_str] {
+        set mul_matches [get_mul_matches_b $in_str 1]
+        set filtered_mul_matches [filter_mul_matches_b $mul_matches]
+        if {[llength $mul_matches] != [llength $filtered_mul_matches]} {
+            puts "  Line $ln_cnt: One or more entries filtered ..."
+        }
+        set mul_result [process_mul_matches_b $filtered_mul_matches]
+        #puts "<$ln_cnt>: $mul_result {$in_str}"
+        puts "<$ln_cnt>: $mul_result"
         incr mul_results $mul_result
     }
     puts "mul_results: $mul_results"
